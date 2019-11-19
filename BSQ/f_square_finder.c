5fa69b7302092e8758180108f847c92e3e5d5b8b
rlfebvr <lefebvre.renaud27@gmail.com>
2019-11-19 19:35:41 +0100

    find square done

--- BSQ/f_square_finder.c
+++ BSQ/f_square_finder.c
@@ -1,21 +1,53 @@
 #include "bsq.h"
 
-t_square	test_bigger(int **map, int line, int col, t_square bigsquare)
+t_square	*test_bigger(int **map, int line, int col, t_square *bigsquare)
 {
+	int		algo;
+	int		tsize;
 
+	algo = 0;
+	tsize = bigsquare->size;
+	if (col + bigsquare->size >= map[0][0] || line + bigsquare->size >= map[0][1])
+		return (NULL);
+	algo = map[line][col] + map[line + tsize][col + tsize];
+	algo = algo - (map[line][col + tsize] + map[line + tsize][col]);
+	if (algo > 0)
+		return (NULL);
+	return (bigsquare);
 }
 
-t_square	ft_is_it_bigger(int **map, int line, int col, t_square bigsquare)
+t_square	*ft_is_it_bigger(int **map, int line, int col, t_square *bigsquare)
 {
-
+	if (col + bigsquare->size >= map[0][0] || line + bigsquare->size >= map[0][1])
+		return (bigsquare);
+	if (map[1][0] == 1)
+		return(bigsquare);
+	if (col > 0)
+		if (map[line][col - 1] < map[line][col])
+			return (bigsquare);
+	if (line > 1)
+		if (map[line - 1][col] < map[line][col])
+			return (bigsquare);
+	while (test_bigger(map, line, col, bigsquare))
+	{
+		bigsquare->colpos = col;
+		bigsquare->lignepos = line;
+		bigsquare->size = bigsquare->size + 1;
+	}	
+	return (bigsquare);
 }
 
-t_square	square_finder(int **map)
+t_square	*square_finder(int **map)
 {
 	int			line;
 	int			col;
-	t_square	bigsquare;
+	t_square	*bigsquare;
+	t_square	square;
 
+	square.colpos = 0;
+	square.lignepos = 1;
+	square.size = 0;
+	bigsquare = &square;
 	line = 0;
 	while (++line <= map[0][0])
 	{
